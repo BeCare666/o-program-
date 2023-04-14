e3f539df0eb2b1d83011cbb5650263285355cb7d
BeCare666 <becare.fr.ge@gmail.com>
2023-04-14 13:35:49 +0100

    first commit

---
+++ admin/loginadmin.js
@@ -0,0 +1,23 @@
+var usernamep = "Johnshop"
+var passwordp = "Johnshop123++"
+var tableauTotakeidd = []
+function connect() {
+    const username = document.getElementById('idusername').value;
+    const password = document.getElementById('idpassword').value;
+    document.getElementById('idusername').value= '';
+        document.getElementById('idpassword').value = ''
+    if (username === usernamep && password === passwordp) {
+        //console.log('kodlsjfnsb,')
+        setTimeout(() => {
+            alert("Johnshop s'est bien connecté")
+            var codesecret = "YESDATA2023"
+            tableauTotakeidd.push(codesecret)
+            const lastk = tableauTotakeidd.slice(-1).pop();
+            localStorage.setItem("storageName", lastk);
+            window.location.replace("admin.html")
+        }, 3000)
+    } else {
+        alert("Mot de passe ou nom d'utilisateur incorrect !")
+    }
+
+}
